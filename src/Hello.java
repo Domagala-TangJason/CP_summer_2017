54f414d04be5a4c0d02273f58d38e8bdeec20017
Domagala-TangJason <jasondomagalatang@gmail.com>
2017-03-08 17:19:57 +0100

    Creating Hello World .gitignore application

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+/**
+ * Created by jd75746 on 3/8/2017.
+ */
+public class Hello {
+    // first class project
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
